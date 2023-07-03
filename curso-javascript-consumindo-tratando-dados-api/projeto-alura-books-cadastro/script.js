aca515a257c14c9e7b7bf907691e5c73127f2824
Demetrius Vignati <demetriusvas@gmail.com>
2023-07-03 17:44:53 -0300

    aula 4

--- curso-javascript-consumindo-tratando-dados-api/projeto-alura-books-cadastro/script.js
+++ curso-javascript-consumindo-tratando-dados-api/projeto-alura-books-cadastro/script.js
@@ -11,15 +11,33 @@
 
 // console.log("Tchau tchau");
 
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if(r.erro){
-        throw Error('Esse CEP não existe!')
-    } else 
-        console.log(r)
-})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluído!'))
+// ------------ 1ª forma para funcionar um código assincrono -------------
+// var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+// .then(resposta => resposta.json())
+// .then(r => {
+//     if(r.erro){
+//         throw Error('Esse CEP não existe!')
+//     } else 
+//         console.log(r)
+// })
+// .catch(erro => console.log(erro))
+// .finally(mensagem => console.log('Processamento concluído!'))
 
-console.log(consultaCEP)
+// console.log(consultaCEP)
+
+// ------------ 2ª forma para funcionar um código assincrono -------------
+async function buscaEndereco(cep){
+    try{
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json();
+        if(consultaCEPConvertida.erro){
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida)
+        return consultaCEPConvertida;
+    } catch(erro){
+        console.log(erro)
+    }
+
+}
+
